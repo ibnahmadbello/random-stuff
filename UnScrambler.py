62eb7081889401e1ba87c56663bf8125123c701c
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-05-06 12:01:16 +0100

    Shuffling of word is ok.

--- UnScrambler.py
+++ UnScrambler.py
@@ -3,30 +3,50 @@
 
 import random
 
+scrambled_word = input("Enter the scrambled word here:\n")
+scrambled_length = len(scrambled_word)
+original_word = ""
+
 def main():
-    scrambled_word = input("Enter the scrambled word here:\n")
-    scrambled_length = len(scrambled_word)
-    original_word = ""
+    word = getRandomWord()
+    print(word)
 
+# # for letters in scrambled_word:
+# def trial(word, new):
+#     for letters in word:
+#         position = random.randrange(len(word))
+#         new += word[position]
+#         word = word[:position] + word[(position + 1):]
+#         if len(new) == scrambled_length:
+#             print(new, end="\t")
+#
+#
+# for i in range():
+#     trial(scrambled_word, original_word)
+# input("\nPress Enter key to exit.")
+#
+#
+# def numberOfPossiblePermutation(scrambled_length):
+#     result = 1
+#     while scrambled_length > 1:
+#         result *= scrambled_length
+#         --scrambled_length
+#     return result
 
-# for letters in scrambled_word:
-def trial(word, new):
-    for letters in word:
-        position = random.randrange(len(word))
-        new += word[position]
-        word = word[:position] + word[(position + 1):]
-        if len(new) == scrambled_length:
-            print(new, end="\t")
+def getRandomWord():
+    newWord = list(scrambled_word)
+    random.shuffle(newWord)
+    return ''.join(newWord)
 
+def loadDictionary():
+    dictionaryFile = open('dictionary.txt')
+    englishWords = {}
+    for word in dictionaryFile.read().split('\n'):
+        englishWords[word] = None
+    dictionaryFile.close()
+    return englishWords
 
-for i in range(numberOfPossiblePermutation(scrambled_length)):
-    trial(scrambled_word, original_word)
-input("\nPress Enter key to exit.")
+ENGLISH_WORDS = loadDictionary()
 
-
-def numberOfPossiblePermutation(scrambled_length):
-    result = 1
-    while scrambled_length > 1:
-        result *= scrambled_length
-        --scrambled_length
-    return result
+if __name__ == '__main__':
+    main()
